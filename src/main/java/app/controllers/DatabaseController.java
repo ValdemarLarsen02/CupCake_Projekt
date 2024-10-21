11e49e7b94cb67146be2553cf674a6d50dc3ab72
Valdemar <41428728+ValdemarLarsen@users.noreply.github.com>
2024-10-21 23:03:14 +0200

    Fikset datbasen forbindelse

---
+++ src/main/java/app/controllers/DatabaseController.java
@@ -0,0 +1,42 @@
+package app.controllers;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseController {
+    private Connection connection;
+
+
+    public DatabaseController() {
+        // Læser vores oplysninger fra .env filen | Fjernet fra github så kode mm ikke bliver leaket.
+        Dotenv dotenv = Dotenv.load();
+
+        String dbUrl = dotenv.get("DB_URL");
+        String dbUsername = dotenv.get("DB_USERNAME");
+        String dbPassword = dotenv.get("DB_PASSWORD");
+
+        try {
+            connection = DriverManager.getConnection(dbUrl, dbUsername, dbPassword);
+            System.out.println("Forbindelse til databasen blev oprettet.");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+
+    public void closeConnection() {
+        try {
+            if (connection != null && !connection.isClosed()) {
+                connection.close();
+                System.out.println("Forbindelsen til databasen blev lukket.");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
